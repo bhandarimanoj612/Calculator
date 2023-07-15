c09ef3bf21e3b05685e93b730da68acc54eec688
manoozder <bhandarimanoj612@gmail.com>
2023-07-15 17:41:18 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// getting input
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+// for string result empty string
+let string = "";
+// for getting all buttons we create array
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
